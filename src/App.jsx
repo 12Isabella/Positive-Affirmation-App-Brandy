52170e34f55d6547b25371064e39d7d0ca4852ba
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-08-29 19:36:40 +0200

    fetch random affirmation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import "./App.css";
+import affirmations from "./affirmations";
+import { useState } from "react";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function App() {
+  let [currentAffirmation, setCurrentAffirmation] = useState(null);
+  function showAffirmation(event) {
+    event.preventDefault();
+    const numAffirmations = affirmations.length;
+    const index = getRandomInt(numAffirmations);
+    setCurrentAffirmation(affirmations[index]);
+  }
   return (
     <div className="App">
       <h1 className="mt-5">Want to feel better? Press the button!</h1>
-      <button className="btn btn-primary mt-5">Get Affirmation!</button>
+      <button onClick={showAffirmation} className="btn btn-primary mt-5">
+        Get Affirmation!
+      </button>
+      <h2 className="mt-3">{currentAffirmation}</h2>
     </div>
   );
 }
