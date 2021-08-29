64a202088854346563b3764253097074b01464af
12Isabella <81431189+12Isabella@users.noreply.github.com>
2021-08-29 18:35:16 +0200

    rename App.js file/ set up page structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="mt-5">Want to feel better? Press the button!</h1>
+      <button className="btn btn-primary mt-5">Get Affirmation!</button>
+    </div>
+  );
+}
+
+export default App;
